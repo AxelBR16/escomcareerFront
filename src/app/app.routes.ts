d99bf96f8682ab95b76ad405ebbd7a4fd7eb0d72
AxelBR16 <117136772+AxelBR16@users.noreply.github.com>
2025-01-29 12:25:19 -0600

    Inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import path from 'path';
+import { InicioComponent } from './inicio/inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: InicioComponent},
+  { path: '**', redirectTo: '', pathMatch:'full'},
+];
