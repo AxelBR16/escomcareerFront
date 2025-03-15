d42f920c4765a5f29fb049b736cc9599b4236c3b
AxelBR16 <117136772+AxelBR16@users.noreply.github.com>
2025-03-14 18:08:49 -0600

    NETLIFY config

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -5,4 +5,19 @@ export const serverRoutes: ServerRoute[] = [
     path: '**',
     renderMode: RenderMode.Prerender
   }
+  ,
+  {
+    path: ':tipo/preguntas/:id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'instrucciones/:tipo',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'carreras/:id',
+    renderMode: RenderMode.Client
+  },
+
+
 ];
