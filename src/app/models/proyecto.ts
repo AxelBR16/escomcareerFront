0212e22ca5796336bf6fd665d98dba8a7ddf702f
AxelBR16 <117136772+AxelBR16@users.noreply.github.com>
2025-05-23 21:07:34 -0600

    subir proyectos ok

---
+++ src/app/models/proyecto.ts
@@ -0,0 +1,10 @@
+export interface proyecto {
+  nombre: string;
+  descripcion: string;
+  url: string;
+  likes: number;
+  dislikes: number;
+  egresadoEmail: string;
+  carreraId: number;
+  materiaId: number;
+}
