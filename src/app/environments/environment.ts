c1d2b0575afc67237e766237af78e771e76f258d
AxelBR16 <117136772+AxelBR16@users.noreply.github.com>
2025-05-27 16:44:48 -0600

    Model Api IA - Aptitudes

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
   apiUrls: {
     /*auth: 'http://localhost:8080'*/
-    auth: 'https://escomcareer.site'
+    auth: 'https://escomcareer.site',
+    modelIA: 'https://3gcsxigrl3.execute-api.us-east-2.amazonaws.com/dev'
   }
 };
--- src/app/services/model.service.ts
+++ src/app/services/model.service.ts
@@ -2,17 +2,18 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { PredictionResponse } from '../models/PredictionResponse';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ModelService {
 
-  private apiUrl = 'https://j4rq1nt78j.execute-api.us-east-2.amazonaws.com/dev';
+
 
   constructor(private http: HttpClient) { }
 
   predictCareer(features: number[]): Observable<PredictionResponse> {
-    return this.http.post<PredictionResponse>(this.apiUrl, { features });
+    return this.http.post<PredictionResponse>(`${environment.apiUrls.modelIA}/aptitudes`, { features });
   }
 }
